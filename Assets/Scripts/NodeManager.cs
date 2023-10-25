9570cb52825966c8991c6baebdb5b32871faaed1
yuxin-miao <yuxinmiao@sjtu.edu.cn>
2023-10-25 13:35:04 +0200

    Ensure newly initialized nodes not overlap with existing nodes

--- Assets/Scripts/NodeManager.cs
+++ Assets/Scripts/NodeManager.cs
@@ -9,6 +9,9 @@ public class NodeManager : MonoBehaviour
   public GameObject processingPopup;  // A GameObject containing the UI for the "Processing..." popup.
 
 
+  // Max attempts to reposition a new node if it overlaps with other nodes
+  private const int maxPositionAttempts = 100;
+
   private GameObject selectedNode = null;
 
   void Start()
@@ -20,11 +23,47 @@ public class NodeManager : MonoBehaviour
     processingPopup.SetActive(false);
   }
 
-  void AddNode()
+  public void AddNode()
   {
-    Vector2 spawnPosition = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width / 2, Screen.height / 2));
-    Instantiate(nodePrefab, spawnPosition, Quaternion.identity);
+    // Get the center of the screen
+    Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+    Vector3 newPosition = Camera.main.ScreenToWorldPoint(screenCenter);
+    newPosition.z = 0;  // Ensure it's on the same Z plane as your other nodes
+
+    // Ensure the new node does not overlap with existing nodes
+    if (IsPositionClear(newPosition))
+    {
+      Instantiate(nodePrefab, newPosition, Quaternion.identity);
+    }
+    else
+    {
+      int attempts = 0;
+      while (attempts < maxPositionAttempts)
+      {
+        // Random screen position offset from the center of the screen
+        Vector3 randomScreenOffset = new Vector3(Random.Range(-Screen.width / 2, Screen.width / 2), Random.Range(-Screen.height / 2, Screen.height / 2), 0);
+        Vector3 randomScreenPosition = screenCenter + randomScreenOffset;
+
+        // Convert that random screen position to world space
+        Vector3 tryPosition = Camera.main.ScreenToWorldPoint(randomScreenPosition);
+        tryPosition.z = 0;
+
+        if (IsPositionClear(tryPosition))
+        {
+          Instantiate(nodePrefab, tryPosition, Quaternion.identity);
+          break;
+        }
+
+        attempts++;
+      }
+
+      if (attempts == maxPositionAttempts)
+      {
+        Debug.LogWarning("Could not find a clear position for the new node.");
+      }
+    }
   }
+
 
   void Update()
   {
@@ -53,5 +92,22 @@ public class NodeManager : MonoBehaviour
     // After this, you'd probably also want to hide the processing popup at some point.
     // You can use a coroutine to simulate a delay, or you might hide it once actual processing is done.
     Debug.Log("User input finished!");
+  }
+
+  // Check if a given position is clear of nodes
+  private bool IsPositionClear(Vector3 position)
+  {
+    Vector3 halfSize = nodePrefab.transform.localScale / 2; // Assuming the node has default scale when instantiated
+    Collider2D[] overlappingColliders = Physics2D.OverlapAreaAll(position - halfSize, position + halfSize);
+
+    foreach (Collider2D collider in overlappingColliders)
+    {
+      if (collider.CompareTag("Node"))
+      {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
